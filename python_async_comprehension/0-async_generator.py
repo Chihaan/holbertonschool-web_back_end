abc461bb68d1cd055eef06e6670d83fcd613f5ef
Chihaan <gavet.vadim@gmail.com>
2026-06-18 10:17:01 +0200

    try fix: file 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """Yield ten random numbers between 0 and 10, one per second."""
-    async for i in range(10):
-        time = random.uniform(0, 10)
+    for _ in range(10):
         await asyncio.sleep(1)
+        time = random.uniform(0, 10)
         yield time
